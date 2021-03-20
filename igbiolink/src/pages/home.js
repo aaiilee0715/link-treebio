3c4598d047b3aaad406bc4cb81b95835cd2f50ff
aaiilee0715 <achansylee@outlook.com>
2021-03-19 21:55:35 -0700

    Final push

---
+++ igbiolink/src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/style.css';
+
+import MainPageDisplay from '../components/homeCard';
+
+
+const MainPage = () => {
+    return (
+        <>
+        <MainPageDisplay />
+        </>
+    );
+}
+export default MainPage;
